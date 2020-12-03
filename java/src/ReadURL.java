31985bf31ee2a3285a865cafd73ca5f00c173612
Patrick Smith <posmith@gmail.com>
2020-12-03 07:32:13 -0500

    added sessionkeys to input and .gitignore

--- java/src/ReadURL.java
+++ java/src/ReadURL.java
@@ -4,9 +4,9 @@ import java.io.*;
 public class ReadURL {
     public static void main(String[] args) throws Exception {
 
-        URL oracle = new URL("https://adventofcode.com/2020/day/2/input");
+        URL url = new URL("https://adventofcode.com/2020/day/2/input");
         BufferedReader in = new BufferedReader(
-        new InputStreamReader(oracle.openStream()));
+        new InputStreamReader(url.openStream()));
 
         String inputLine;
         while ((inputLine = in.readLine()) != null)
