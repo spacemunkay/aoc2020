480135f7aff7e7637352afc987a945d9b02c8685
Patrick Smith <posmith@gmail.com>
2021-01-06 07:06:35 -0500

    Day 17 solved.

--- java/src/Day17.java
+++ java/src/Day17.java
@@ -4,25 +4,130 @@ import java.util.List;
 public class Day17 {
 
 	public static void main(String[] args) throws Exception {
-		//StringBuffer sb = ReadURL.getWebData(17, "patrick");
-		//String data = sb.toString();
-		String test = ".#.\n" + "..#\n" + "###";
-		String single = "#";
-		String triple = "###\n###\n###\n\n###\n#.#\n###\n\n###\n###\n###";
-		List<List<List<Character>>> cube = createCube(test);
-		cube = cycleMulti(cube, 6);
+		StringBuffer sb = ReadURL.getWebData(17, "jason");
+		String data = sb.toString();
+		List<List<List<Character>>> cube = createCube(data);
+		List<List<List<List<Character>>>> tesseract = createHypercube(data);
+		System.out.println("first answer = " + cycleMulti(cube, 6));
+		System.out.println("second answer = " + cycleMulti4(tesseract, 6));
 
 	}
 
-	private static List<List<List<Character>>> cycleMulti(List<List<List<Character>>> cube, int cycles) {
-		System.out.println("Before any cycles:\n");
-		printCube(cube);
+	private static int cycleMulti4(List<List<List<List<Character>>>> tesseract, int cycles) {
 		for (int i = 1; i <= cycles; i++) {
-			System.out.println("After " + i + " cycles:\n");
-			cube = nextCycle(cube);
-			System.out.println(countActive(cube));
+			tesseract = nextCycle4(tesseract);
 		}
-		return cube;
+		return countActive4(tesseract);
+	}
+
+	private static List<List<List<List<Character>>>> nextCycle4(List<List<List<List<Character>>>> tesseract) {
+		tesseract = enlargenTesseract(tesseract);
+		List<List<List<List<Character>>>> newTess = new ArrayList<List<List<List<Character>>>>();
+		for (int h = 0; h < tesseract.size(); h++) {
+			List<List<List<Character>>> newCube = new ArrayList<List<List<Character>>>();
+			for (int i = 0; i < tesseract.get(0).size(); i++) {
+				List<List<Character>> newLayer = new ArrayList<List<Character>>();
+				for (int j = 0; j < tesseract.get(0).get(0).size(); j++) {
+					List<Character> newRow = new ArrayList<Character>();
+					for (int k = 0; k < tesseract.get(0).get(0).get(0).size(); k++) {
+						int neighbors = countNeighbors(tesseract, h, i, j, k);
+						if (neighbors == 3 || (neighbors == 2 && tesseract.get(h).get(i).get(j).get(k) == '#')) {
+							newRow.add('#');
+						} else {
+							newRow.add('.');
+						}
+					}
+					newLayer.add(newRow);
+				}
+				newCube.add(newLayer);
+			}
+			newTess.add(newCube);
+		}
+		return newTess;
+	}
+
+	private static int countNeighbors(List<List<List<List<Character>>>> tesseract, int h, int i, int j, int k) {
+		int sum = 0;
+		List<List<Integer>> offsets = new ArrayList<List<Integer>>();
+		offsets = getOffsets(h, i, j, k);
+		for (List<Integer> offset : offsets) {
+			sum += count(tesseract, offset.get(0), offset.get(1), offset.get(2), offset.get(3));
+		}
+		return sum;
+	}
+
+	private static int count(List<List<List<List<Character>>>> tesseract, int h, int i, int j, int k) {
+		try {
+			return tesseract.get(h).get(i).get(j).get(k) == '#' ? 1 : 0;
+		} catch (IndexOutOfBoundsException e) {
+			return 0;
+		}
+	}
+
+	private static List<List<Integer>> getOffsets(int w, int x, int y, int z) {
+		List<List<Integer>> offsets = new ArrayList<List<Integer>>();
+		for (int h = w - 1; h <= w + 1; h++)
+			for (int i = x - 1; i <= x + 1; i++) {
+				for (int j = y - 1; j <= y + 1; j++) {
+					for (int k = z - 1; k <= z + 1; k++) {
+						if (h != w || i != x || j != y || k != z) {
+							List<Integer> coords = new ArrayList<Integer>();
+							coords.add(h);
+							coords.add(i);
+							coords.add(j);
+							coords.add(k);
+							offsets.add(coords);
+						}
+					}
+				}
+			}
+		return offsets;
+	}
+
+	private static List<List<List<List<Character>>>> enlargenTesseract(List<List<List<List<Character>>>> tesseract) {
+		List<List<List<List<Character>>>> biggerTess = new ArrayList<List<List<List<Character>>>>();
+		for (int h = -1; h < tesseract.size() + 1; h++) {
+			List<List<List<Character>>> biggerCube = new ArrayList<List<List<Character>>>();
+			for (int i = -1; i < tesseract.get(0).size() + 1; i++) {
+				List<List<Character>> biggerLayer = new ArrayList<List<Character>>();
+				for (int j = -1; j < tesseract.get(0).get(0).size() + 1; j++) {
+					List<Character> biggerRow = new ArrayList<Character>();
+					for (int k = -1; k < tesseract.get(0).get(0).get(0).size() + 1; k++) {
+						try {
+							biggerRow.add(tesseract.get(h).get(i).get(j).get(k));
+						} catch (IndexOutOfBoundsException e) {
+							biggerRow.add('.');
+						}
+					}
+					biggerLayer.add(biggerRow);
+				}
+				biggerCube.add(biggerLayer);
+			}
+			biggerTess.add(biggerCube);
+		}
+		return biggerTess;
+	}
+
+	private static int countActive4(List<List<List<List<Character>>>> tesseract) {
+		int sum = 0;
+		for (List<List<List<Character>>> cube : tesseract) {
+			sum += countActive(cube);
+		}
+		return sum;
+	}
+
+	private static List<List<List<List<Character>>>> createHypercube(String data) {
+		List<List<List<List<Character>>>> tesseract = new ArrayList<List<List<List<Character>>>>();
+		List<List<List<Character>>> cube = createCube(data);
+		tesseract.add(cube);
+		return tesseract;
+	}
+
+	private static int cycleMulti(List<List<List<Character>>> cube, int cycles) {
+		for (int i = 1; i <= cycles; i++) {
+			cube = nextCycle(cube);
+		}
+		return countActive(cube);
 	}
 
 	private static int countActive(List<List<List<Character>>> cube) {
@@ -62,32 +167,31 @@ public class Day17 {
 	}
 
 	private static int countNeighbors(List<List<List<Character>>> cube, int i, int j, int k) {
-		return count(cube, i, j, k - 1) +
-				count(cube, i, j, k + 1) +
-				count(cube, i, j - 1, k) +
-				count(cube, i, j - 1, k - 1) +
-				count(cube, i, j - 1, k + 1) +
-				count(cube, i, j + 1, k) +
-				count(cube, i, j + 1, k - 1) +
-				count(cube, i, j + 1, k + 1) +
-				count(cube, i - 1, j, k) +
-				count(cube, i - 1, j, k - 1) +
-				count(cube, i - 1, j, k + 1) +
-				count(cube, i - 1, j - 1, k) +
-				count(cube, i - 1, j - 1, k - 1) +
-				count(cube, i - 1, j - 1, k + 1) +
-				count(cube, i - 1, j + 1, k) +
-				count(cube, i - 1, j + 1, k - 1) +
-				count(cube, i - 1, j + 1, k + 1) +
-				count(cube, i + 1, j, k) +
-				count(cube, i + 1, j, k - 1) +
-				count(cube, i + 1, j, k + 1) +
-				count(cube, i + 1, j - 1, k) +
-				count(cube, i + 1, j - 1, k - 1) +
-				count(cube, i + 1, j - 1, k + 1) +
-				count(cube, i + 1, j + 1, k) +
-				count(cube, i + 1, j + 1, k - 1) +
-				count(cube, i + 1, j + 1, k + 1);
+		int sum = 0;
+		List<List<Integer>> offsets = new ArrayList<List<Integer>>();
+		offsets = getOffsets(i, j, k);
+		for (List<Integer> offset : offsets) {
+			sum += count(cube, offset.get(0), offset.get(1), offset.get(2));
+		}
+		return sum;
+	}
+
+	private static List<List<Integer>> getOffsets(int x, int y, int z) {
+		List<List<Integer>> offsets = new ArrayList<List<Integer>>();
+		for (int i = x - 1; i <= x + 1; i++) {
+			for (int j = y - 1; j <= y + 1; j++) {
+				for (int k = z - 1; k <= z + 1; k++) {
+					if (i != x || j != y || k != z) {
+						List<Integer> coords = new ArrayList<Integer>();
+						coords.add(i);
+						coords.add(j);
+						coords.add(k);
+						offsets.add(coords);
+					}
+				}
+			}
+		}
+		return offsets;
 	}
 
 	private static int count(List<List<List<Character>>> cube, int i, int j, int k) {
@@ -116,20 +220,6 @@ public class Day17 {
 			biggerCube.add(biggerLayer);
 		}
 		return biggerCube;
-
-	}
-
-	private static void printCube(List<List<List<Character>>> cube) {
-		for (int i = 0; i < cube.size(); i++) {
-			System.out.println("z = " + (i - cube.size() / 2));
-			for (int j = 0; j < cube.get(0).size(); j++) {
-				for (int k = 0; k < cube.get(0).get(0).size(); k++) {
-					System.out.print(cube.get(i).get(j).get(k));
-				}
-				System.out.println();
-			}
-			System.out.println();
-		}
 	}
 
 	private static List<List<List<Character>>> createCube(String data) {
